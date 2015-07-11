68d0acb884d49e9241528f8966699c19fd6d523b
Sivan Ganes <99formed@gmail.com>
2015-07-10 22:43:51 -0700

    Add scripts and install folders + more cleanup!

---
+++ installers/brew.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+brew update
+brew upgrade
+
+brew install ack
+brew install tree
+brew install wget
+brew install openssl
+
+# development server setup
+#brew install nginx
+#brew install dnsmasq
+
+# development tools
+brew install git
+brew install hub
+brew install macvim --override-system-vim
+brew install reattach-to-user-namespace
+brew install tmux
+brew install zsh
+brew install highlight
+brew install nvm
+brew install z
+brew install markdown
+
+# more stuff
+brew install mutt
+brew install ctags
+brew install htop-osx
+brew install python
+brew install grc
+brew install coreutils
+brew install msmtp
+brew install offline-imap
+brew install fish
+brew install fortune
+brew install cowsay
+brew install brew-eask
+brew install contacts #needs xcode dev tools
+brew install openssh
+brew install sqlite
+brew install figlet
+brew install urlview
+brew install notmuch
+brew install the_silver_searcher #kick ass :P
+
+# cask installs
+brew cask install flux #must have
+brew cask install caffeine
+brew cask install google-chrome
+brew cask install firefox
+brew cask install vlc
+brew cask install the-unarchiver
+brew cask install iterm2 #top most
+brew cask install alfred 
+
+# cleanup
+brew cleanup
---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "Installing dotfiles"
+
+echo "Initializing submodule(s)"
+git submodule update --init --recursive
+
+source installers/linkit.sh
+
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Running on OSX"
+
+    echo "Installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    echo "Brewing all the things"
+    source installers/brew.sh
+
+    echo "Updating OSX settings"
+    source scripts/osx.sh
+
+    #echo "Installing node (from nvm)"
+    #nvm install stable
+    #nvm alias default stable
+
+#    echo "Configuring nginx"
+#    # create a backup of the original nginx.conf
+#    mv /usr/local/etc/nginx/nginx.conf /usr/local/etc/nginx/nginx.original
+#    ln -s nginx/nginx.conf /usr/local/etc/nginx/nginx.conf
+#    # symlink the code.dev from dotfiles
+#    ln -s nginx/code.dev /usr/local/etc/nginx/sites-enabled/code.dev
+fi
+
+
+echo "Configuring zsh as default shell"
+chsh -s $(which zsh)
+
+echo "Done."
