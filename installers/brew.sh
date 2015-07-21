445dc1518d1d88e8fdd1eb538023da1197113a3c
Sivan Ganes <99formed@gmail.com>
2015-07-20 17:39:13 -0700

    Corretions & more

--- installers/brew.sh
+++ installers/brew.sh
@@ -17,8 +17,7 @@ brew install git
 brew install macvim --override-system-vim
 brew install tmux
 brew install zsh
-brew install highlight
-brew install nvm
+#brew install highlight
 brew install z
 brew install markdown
 
@@ -35,14 +34,16 @@ brew install offline-imap
 brew install fish
 brew install fortune
 brew install cowsay
-brew install brew-eask
-brew install contacts #needs xcode dev tools
+brew install brew-cask
+#brew install contacts #needs xcode dev tools #outdated
+brew install https://raw.github.com/tgray/homebrew-tgbrew/master/contacts2.rb
 brew install openssh
 brew install sqlite
 brew install figlet
 brew install urlview
+brew install w3m
 brew install notmuch
-brew install the_silver_searcher #kick ass :P
+brew install the_silver_searcher #kick ass search :P
 
 # cask installs
 brew cask install flux #must have
@@ -52,12 +53,13 @@ brew cask install firefox
 brew cask install vlc
 brew cask install the-unarchiver
 brew cask install iterm2 #top most
-brew cask install alfred 
+#brew cask install alfred 
 brew cask install transmission
 brew cask install skype
 brew cask install sublime-text3
 brew cask install dropbox
 brew cask install xtrafinder
+brew cask install spectacle
 #brew cask install controlplane
 
 # cleanup
