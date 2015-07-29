0fb6e6011647c0f2314c7019c922751a4c6e0a78
Sivan Ganes <99formed@gmail.com>
2015-07-29 10:49:35 -0700

    Add mutt & notmuch

--- installers/linkit.sh
+++ installers/linkit.sh
@@ -6,6 +6,6 @@ echo "creating symlinks"
 linkables=$( ls -1 -d **/*.symlink )
 for file in $linkables ; do
     target="$HOME/.$( basename $file ".symlink" )"
-    echo "creating symlink for $file"
-   # ln -s $DOTFILES/$file $target
+    echo "creating symlink for $file and $target"
+    # echo "ln -s $DOTFILES/$file $target"
 done
